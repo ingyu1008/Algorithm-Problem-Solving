3e8e502bab34043a9f9347e7217c17579e6fef91
ingyu1008 <ingyu1008@gmail.com>
2021-04-05 02:39:23 +0900

    ADD : BOJ 21293 No Description 2

---
+++ Baekjoon Online Judge/No Description 2/solution.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long long c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, A, B;
+    scanf("%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld%lld", &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &c, &B, &A);
+    printf("%lld\n%lld\n%lld\n", A - B, A * B, A + B + 1);
+    return 0;
+}
