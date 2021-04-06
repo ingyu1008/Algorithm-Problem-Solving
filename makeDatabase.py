de650ff492b58097577d543c4f430217ee2145b7
ingyu1008 <ingyu1008@gmail.com>
2021-04-06 22:48:22 +0900

    UPD : get recent solves

--- makeDatabase.py
+++ makeDatabase.py
@@ -2,10 +2,6 @@ import requests  # http 요청을 위한 모듈
 import json  # json 파일을 다루기 위한 모듈
 
 # 두 딕셔너리 병합
-
-def getProblemId(problem):
-    return json.loads(problem)["id"]
-
 def merge(old, new):
     if len(old) == 0:
         return new
@@ -15,10 +11,21 @@ def merge(old, new):
 
     return old
 
+# 문제 번호 반환 
+def getProblemId(problem):
+    return problem["id"]
 
+# 새롭게 추가된 문제 확인
 def calculateDiff(old, new):
-    o = old["result"]["problems"]
-    n = new["result"]["problems"]
+    try:
+        o = old.DB["result"]["problems"]
+    except KeyError:
+        o = []
+    
+    try:
+        n = new.DB["result"]["problems"]
+    except KeyError:
+        n = []
 
     ptro = 0
     ptrn = 0
@@ -29,18 +36,36 @@ def calculateDiff(old, new):
         a = 2147483647
         b = 2147483647
 
-        if ptro < len(o) :
-            a = getProblemId(o[ptro]) 
-        if ptrn < len(n) :
-            b = getProblemId(o[ptrn])
+        if ptro < len(o):
+            a = getProblemId(o[ptro])
+        if ptrn < len(n):
+            b = getProblemId(n[ptrn])
 
         if a >= b:
             ptrn += 1
         if a <= b:
             ptro += 1
         if a != b:
-            diff.add(min(a,b))
+            diff.add(min(a, b))
 
+    # diff.json 이라는 임시 파일로 저장
+    with open("diff.json", "w") as out:
+        tmp = dict()
+        tmp["problems"] = list(diff)[:100]
+        json.dump(tmp, out, indent='\t')
+        out.close()
+
+# 최근 Solve 업데이트
+def updateRecent(recent, diff):
+    if not "problems" in recent.DB.keys():
+        recent.DB["problems"] = []
+
+    for item in diff.DB["problems"]:
+        if not item in recent.DB["problems"]:
+            recent.DB["problems"].insert(0,item)
+
+    while len(recent.DB["problems"]) > 100:
+        recent.DB["problems"].pop()
 
 # Database
 class Database:
@@ -53,7 +78,7 @@ class Database:
                 self.DB = json.load(f)
                 f.close()
         except:
-            print("No DB to read")
+            print(f"No DB to read : {file}")
 
     # Write New DB
     def writeDB(self, file="database.json"):
@@ -88,3 +113,14 @@ if __name__ == "__main__":
     newDB.writeDB()
 
     calculateDiff(oldDB, newDB)
+    diff = Database()
+    diff.readDB('diff.json')
+
+    recent = Database()
+    recent.readDB("recent.json")
+    
+    updateRecent(recent, diff)
+    recent.writeDB("recent.json")
+
+
+
