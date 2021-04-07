c1eb5a8eb084e7b5b6c5e0e45181ed8629054434
ingyu1008 <ingyu1008@gmail.com>
2021-04-07 14:41:12 +0900

    UPD : Change Algorithm

--- makeDatabase.py
+++ makeDatabase.py
@@ -1,15 +1,6 @@
 import requests  # http 요청을 위한 모듈
 import json  # json 파일을 다루기 위한 모듈
-
-# 두 딕셔너리 병합
-def merge(old, new):
-    if len(old) == 0:
-        return new
-
-    for item in new["result"]["problems"]:
-        old["result"]["problems"].append(item)
-
-    return old
+from Database import Database
 
 # 문제 번호 반환 
 def getProblemId(problem):
@@ -30,7 +21,7 @@ def calculateDiff(old, new):
     ptro = 0
     ptrn = 0
 
-    diff = set()
+    diff = list()
 
     while ptro < len(o) or ptrn < len(n):
         a = 2147483647
@@ -41,17 +32,20 @@ def calculateDiff(old, new):
         if ptrn < len(n):
             b = getProblemId(n[ptrn])
 
-        if a >= b:
-            ptrn += 1
-        if a <= b:
+        if a == b:
             ptro += 1
-        if a != b:
-            diff.add(min(a, b))
+            ptrn += 1
+        if a < b:
+            diff.append(o[ptro])
+            ptro += 1
+        if a > b:
+            diff.append(n[ptrn])
+            ptrn += 1
 
     # diff.json 이라는 임시 파일로 저장
     with open("diff.json", "w") as out:
         tmp = dict()
-        tmp["problems"] = list(diff)[:100]
+        tmp["problems"] = diff[:100]
         json.dump(tmp, out, indent='\t')
         out.close()
 
@@ -66,44 +60,6 @@ def updateRecent(recent, diff):
 
     while len(recent.DB["problems"]) > 100:
         recent.DB["problems"].pop()
-
-# Database
-class Database:
-    DB = dict()
-
-    # Read Old DB
-    def readDB(self, file='database.json'):
-        try:
-            with open(file, 'r') as f:
-                self.DB = json.load(f)
-                f.close()
-        except:
-            print(f"No DB to read : {file}")
-
-    # Write New DB
-    def writeDB(self, file="database.json"):
-        with open(file, 'w') as f:
-            json.dump(self.DB, f, indent='\t')
-            f.close()
-
-    # Save All Solved Problems on DB
-    def getSolvedProblems(self, userId="ingyu1008", page=1):
-        total = 1
-
-        while page <= total:
-            URL = f"https://api.solved.ac/v2/search/problems.json?query=solved_by:{userId}&page={page}&sort=level&sort_direction=asc"
-            res = requests.get(URL)
-
-            if res.status_code == 200:
-                self.DB = merge(self.DB, res.json())
-                total = res.json()["result"]["total_page"]
-                print(f"{page} pages out of {total} pages done")
-                page += 1
-            else:
-                print(
-                    f"Error occured while requesting {URL}, Error Code {res.status_code}")
-                break
-
 
 if __name__ == "__main__":
     oldDB = Database()
