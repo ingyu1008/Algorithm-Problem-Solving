8298d6dd9f7c9bc3bf2039357dc0cb5468d4e91e
ingyu1008 <ingyu1008@gmail.com>
2021-04-07 15:11:20 +0900

    FIX : encoding with utf-8

--- parseRecentProbs.py
+++ parseRecentProbs.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     recent = Database()
     recent.readDB("recent.json")
 
-    with open("recent.md", "w") as f:
+    with open("recent.md", "w", encoding="utf-8") as f:
         f.write("| 문제 번호 | 문제 이름 | 난이도 | 풀이 코드 |\n")
         for problem in recent.DB["problems"]:
             id = problem["id"]
