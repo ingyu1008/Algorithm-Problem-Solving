7c044153ca028aa4c34aa7bd9a354d37e23b4b17
ingyu1008 <ingyu1008@gmail.com>
2021-06-22 20:54:07 +0900

    FIX : solved.ac api 변경 픽스

--- Database.py
+++ Database.py
@@ -7,10 +7,31 @@ def merge(old, new):
     if len(old) == 0:
         return new
 
-    for item in new["items"]: # 새로운 api 기준 컬럼명 바뀜
+    for item in new["result"]["problems"]:
         old["result"]["problems"].append(item)
 
     return old
+
+# 새로운 api에 적용
+def newToOld(new, total=1, page=1):
+    tmp = dict()
+    tmp["success"] = True
+    tmp["result"] = dict()
+    tmp["result"]["total_problems"] = total
+    tmp["result"]["total_page"] = page-1
+    tmp["result"]["problems"] = list()
+    for item in new["items"]:
+        dct = dict()
+        dct["id"] = item["problemId"]
+        dct["level"] = item["level"]
+        dct["level_locked"] = 1 if item["isLevelLocked"] else 0
+        dct["solvable"] = 1 if item["isSolvable"] else 0
+        dct["title"] = item["titleKo"]
+        dct["solved_count"] = item["acceptedUserCount"]
+        dct["average_try"] = item["averageTries"]
+        tmp["result"]["problems"].append(dct)
+        
+    return tmp
 
 # Database
 class Database:
@@ -44,14 +65,17 @@ class Database:
             res = requests.get(URL)
 
             if res.status_code == 200:
-                self.DB = merge(self.DB, res.json())
                 total = res.json()["count"]
                 currentCount += len(res.json()["items"])
                 page += 1
+                transformed = newToOld(res.json(), total, page)
+                self.DB = merge(self.DB, transformed)
             else:
                 print(
                     f"Error occured while requesting {URL}, Error Code {res.status_code}")
                 break
+
+        
     
     def getCFStats(self, handle):
         URL = f"https://codeforces.com/api/user.rating?handle={handle}"
